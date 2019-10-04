444c5f95819e6d0ca723a49823581be19bc43c0a
Mauricio Kirchhof Ferret <37005822+maurif1605@users.noreply.github.com>
2019-10-03 23:15:38 -0300

    Create LRU.py

---
+++ LRU.py
@@ -0,0 +1,55 @@
+class LRU:
+    """Algoritmo Least Recently Used (LRU) para substituicao de paginas na memoria RAM"""
+
+    def execute(self, qtd_quadros, referencias):
+        """
+        Executa o algoritmo de substituicao de paginas LRU. Recebe como
+        parametros a quantidade de quadros e a lista de paginas referenciadas.
+        """
+        # Inicializa os valores de miss
+        PageFault = 0 # Quando a pagina referenciada nao esta na memoria
+
+        # Configura o instante inicial como zero
+        instante = 0
+
+        # Quadros da memoria
+        quadros = {} # As chaves sao as paginas e os valores o instante que entrou na memoria
+
+        # Percorre a lista de paginas referenciadas
+        for ref in referencias:
+            if ref not in quadros.keys():
+                PageFault += 1 # incrementa o numero de paginas faltantes
+
+                # Se todos os quadros estiverem ocupados remove a pagina usada a mais tempo
+                if len(quadros) == qtd_quadros:
+                    mais_antiga = self.__pagina_mais_antiga(quadros)
+                    del quadros[mais_antiga] # Retira a pagina da memoria RAM
+
+            # Adiciona a pagina na memoria RAM ou atualiza o instante de utilizacao
+            quadros[ref] = instante
+            instante += 1
+
+        # Retorna o numero de paginas nao encontradas na memoria RAM
+        return PageFault
+
+
+    def __pagina_mais_antiga(self, quadros):
+        """
+        Procura a pagina referenciada a mais tempo na memoria RAM. Recebe como
+        parametros os quadros da memoria.
+        """
+
+        # Considera inicialmente a primeira a mais antiga
+        mais_antiga = quadros.keys()[0]
+        menor_instante = quadros.values()[0]
+
+        # Compara os instantes da ultima utilizacao de cada pagina
+        for chave in quadros.keys():
+            # Se chave for menor que a chame da mais antiga ate o momento
+            # a mais antiga e atualizada
+            if quadros[chave] < menor_instante:
+                mais_antiga = chave
+                menor_instante = quadros[chave]
+
+        # Retorna a chave da pagina referenciada a mais tempo
+        return mais_antiga
